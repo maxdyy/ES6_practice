41df37c7884a2075efcfae7e27b3e2790fc155d2
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-03-05 10:49:35 +0100

    More on let and const

--- variables.js
+++ variables.js
@@ -5,8 +5,47 @@ var width = 100;
 
 // let and cons is scoped to a block (like if conditional) 
 // and is not going to be leaked to global 
-// Let & const cannot be re-declared in the same scope
+// let & const cannot be re-declared in the same scope
 let height = 200;
 
-// we cannot update const variables, but properties like in a const object can be changed 
+// we cannot update const variables, but properties, like in a 
+// const object can be changed
 const key = "abcd";
+
+
+// here the idea is to have i logged every 1 sec and it should be 1, 2, 3
+// and so on
+for(var i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000)
+}
+
+// however the result is going to be all console log of i = 10 after 1 sec
+// because the loop goes though and the var i is being overwritten to 10
+// to fix this, we can change var to let
+
+for(let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000)
+}
+
+// so, since let is bloke scoped after a sec we will have a console log
+// of each i from 0 to 10
+
+
+// Temporal dead zone
+console.log(pizza)
+//
+// this is the temporal dead zone
+// you cannot call pizza before it is defined
+//
+const pizza = '🍕';
+
+/* 
+--- ES6 rules:
+- Use const by default.
+- Only use let if rebinding is needed.
+- No real need to use var in ES6
+*/
